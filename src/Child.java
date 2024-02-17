3074b03a286018a6a50b8356b52a67a4486d2cd6
Shobhith Shobhi <shobhithm824@gmail.com>
2024-02-17 12:54:52 +0530

    Inheritance program

---
+++ src/Child.java
@@ -0,0 +1,7 @@
+public class Child extends Parent{
+
+    void f2() {
+        System.out.println("Inside the child class.");
+    }
+
+}
---
+++ src/MultilevelTest.java
@@ -0,0 +1,9 @@
+import java.lang.Object;
+public class MultilevelTest {
+    public static void main(String args[]) {
+        Child c = new Child();
+        c.f1();
+        c.f2();
+        c.hashCode();
+    }
+}
