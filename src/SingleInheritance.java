0c6326ada34c5855e962da5f04fee29632e3c21c
Shobhith Shobhi <shobhithm824@gmail.com>
2024-02-17 08:44:32 +0530

    Inheritance program

---
+++ src/SingleInheritance.java
@@ -0,0 +1,9 @@
+import java.lang.Object;
+public class SingleInheritance {
+    void m1() {
+        SingleInheritance s = new SingleInheritance();
+        s.m1();
+        s.hashCode();
+        s.getClass();
+    }
+}
