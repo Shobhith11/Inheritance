cea847f94432d78c17d08fe1d8244174608752c1
Shobhith Shobhi <shobhithm824@gmail.com>
2024-02-17 13:12:47 +0530

    Inheritance program

--- src/Child.java
+++ src/Child.java
@@ -1,5 +1,9 @@
 public class Child extends Parent{
 
+    Child() {
+        System.out.println("In Child Object"+this);
+    }
+
     void f2() {
         System.out.println("Inside the child class.");
     }
--- src/Parent.java
+++ src/Parent.java
@@ -1,4 +1,7 @@
 public class Parent {
+    Parent() {
+        System.out.println("In Parent Object"+this);
+    }
     void f1(){
         System.out.println("Inside the Parent class.");
     }
